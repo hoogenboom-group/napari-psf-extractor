7ed3faa65327fad5ef60505ed3a4c279fe882fc4
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-11-29 16:42:22 +0100

    Add input dialog for sliders
    Add input dialog for sliders and also enable slider precision to one
    decimal point.
    
    Issue: https://github.com/hoogenboom-group/napari-psf-extractor/issues/1#issue-1972106649

--- src/napari_psf_extractor/components/sliders.py
+++ src/napari_psf_extractor/components/sliders.py
@@ -1,5 +1,6 @@
-from qtpy.QtWidgets import QLabel, QVBoxLayout, QWidget
+from napari.utils.notifications import show_error
 from qtpy.QtCore import Qt, QTimer
+from qtpy.QtWidgets import QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget
 from superqt import QRangeSlider
 
 
@@ -7,9 +8,11 @@ class RangeSlider(QWidget):
     """
     Range slider widget.
     """
+
     def __init__(self, min_value, max_value, callback):
         """
         Initialize the range slider widget.
+        The slider values are in units of 0.1.
 
         Parameters
         ----------
@@ -22,33 +25,47 @@ class RangeSlider(QWidget):
         """
         super().__init__()
 
+        # Set the range slider values to be in units of 0.1
+        max_value = max_value * 10
+
         self.slider = QRangeSlider(Qt.Orientation.Horizontal)
         self.slider.setRange(min_value, max_value)
         self.slider.setValue((min_value, max_value))
 
-        self.label = QLabel(f"Range: [{min_value}, {max_value}]")
+        self.min_label = QLabel("Min:")
+        self.max_label = QLabel("Max:")
+        self.min_value_edit = QLineEdit(str(min_value))
+        self.max_value_edit = QLineEdit(str(max_value))
+
+        # Layout
+        input_layout = QHBoxLayout()
+        input_layout.addWidget(self.min_label)
+        input_layout.addWidget(self.min_value_edit)
+        input_layout.addWidget(self.max_label)
+        input_layout.addWidget(self.max_value_edit)
+
+        layout = QVBoxLayout()
+        layout.addLayout(input_layout)
+        layout.addWidget(self.slider)
+        self.setLayout(layout)
+
+        # Signals
+        self.slider.valueChanged.connect(self.update_range)
+        self.min_value_edit.editingFinished.connect(self.input_change)
+        self.max_value_edit.editingFinished.connect(self.input_change)
 
         # QTimer to handle delayed updates
         self.timer = QTimer()
         self.timer.setSingleShot(True)
         self.timer.timeout.connect(callback)
 
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.slider)
-        self.setLayout(layout)
-
-        # Signals
-        self.slider.valueChanged.connect(self.update_range)
-
     def update_range(self):
         """
         Update the range label. This function is called when the range slider is moved.
         """
         mass_range = self.slider.value()
-
-        self.label.setText(f"Mass Range: [{mass_range[0]}, {mass_range[1]}]")
+        self.min_value_edit.setText(str(mass_range[0] / 10))
+        self.max_value_edit.setText(str(mass_range[1] / 10))
 
         # Restart the timer
         if self.timer.isActive():
@@ -57,6 +74,23 @@ class RangeSlider(QWidget):
         # Timer used to prevent excessive and rapid updates (32 ms)
         self.timer.start(32)
 
+    def input_change(self):
+        """
+        Update the slider values based on the text input fields.
+        """
+        try:
+            min_value = float(self.min_value_edit.text())
+            max_value = float(self.max_value_edit.text())
+        except ValueError:
+            show_error("Error: Invalid input.")
+            return
+
+        if min_value > max_value:
+            show_error("Error: Min value must be less than or equal to max value.")
+            return
+
+        self.slider.setValue((min_value, max_value))
+
     def reset(self):
         """
         Reset the range slider to its initial state.
@@ -64,4 +98,4 @@ class RangeSlider(QWidget):
         self.slider.setValue((self.slider.minimum(), self.slider.maximum()))
 
     def value(self):
-        return self.slider.value()
+        return self.slider.value()[0] / 10, self.slider.value()[1] / 10
