cf7d76bef967e3affef70009902bad62b77c946c
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-12-24 14:41:24 +0200

    Mock matplotlib figure in plotting tests
    Fix CI/CD by mocking matplotlib figure in plotting tests.

--- src/napari_psf_extractor/_tests/test_plotting.py
+++ src/napari_psf_extractor/_tests/test_plotting.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 import numpy as np
 import pandas as pd
@@ -9,9 +9,14 @@ from ..plotting import plot_mass_range
 
 class TestPlotting(unittest.TestCase):
     @patch('matplotlib.axes.Axes')
-    def test_plot_mass_range_aspect_ratio(self, mock_axes):
+    @patch('matplotlib.pyplot.gcf')
+    def test_plot_mass_range_aspect_ratio(self, mock_gcf, mock_axes):
         # Given
+        mock_fig = Mock()
+        mock_fig.dpi = 1
+        mock_gcf.return_value = mock_fig
         ax = mock_axes
+
         mip = np.ones((10, 10), dtype=np.float64)  # Ensure dtype is float64
         mass = (1, 4)
         features_data = {'raw_mass': [2, 3], 'x': [1, 2], 'y': [3, 4]}
@@ -23,3 +28,7 @@ class TestPlotting(unittest.TestCase):
         # Then
         expected = mip.shape[1] / mip.shape[0]
         ax.set_aspect.assert_called_once_with(expected)
+        mock_fig.set_size_inches.assert_called_once_with(
+            mip.shape[1] / mock_fig.dpi,
+            mip.shape[0] / mock_fig.dpi
+        )
